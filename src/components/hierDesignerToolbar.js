41fb3d463d619f1790d4ee1d71589a77da83e189
raymond-linuxmint <remondo@gmail.com>
2019-12-21 23:42:40 +0800

    Reimplement Hierarchy Toolbar layout

--- src/components/hierDesignerToolbar.js
+++ src/components/hierDesignerToolbar.js
@@ -5,10 +5,24 @@ import './hierDesignerToolbar.css';
 // It is permanently placed on top.
 // Houses the search bar and other buttons
 const HierarchyToolbar = (props) => {
-    return <div className="hierDesignToolbar" style={{width: `${props.containerWidth - 5}px`}}>
+    return <div className="hierDesignToolbar">
         <div className="ui icon input small" style={{margin: "5px", width: '100%'}}>
             <input type="text" placeholder="Search tree..."/>
             <i className="inverted circular filter icon"></i>
+        </div>
+        <div className="toolBarBtnContainer">
+            <div className="toolbarButton">
+            <i className="ui icon save"/>
+            Save
+            </div>
+            <div className="toolbarButton">
+            <i className="ui icon trash alternate"/>
+            Delete
+            </div>
+            <div className="toolbarButton">
+            <i className="ui icon plus insert"/>
+            Insert
+            </div>
         </div>
     </div>
 }
--- src/containers/hierarchyDesigner.js
+++ src/containers/hierarchyDesigner.js
@@ -5,16 +5,33 @@ import HierarchyDesignerTree from './hierearchyTree';
 import HierarchyToolbar from '../components/hierDesignerToolbar';
 
 import './hierarchyTree.css'; //temp only
+import './hierarchyDesigner.css';
+
+const ToolbarPanel = ({title, panelContent}) => {
+    //console.log('[render] AccordionPanel, id: ', id);
+    return <div className="toolbarPanel">        
+        <div className="toolbarContainer">
+            <HierarchyToolbar/>
+        </div>
+        <div className="toolbarPanelBody">
+            {panelContent}
+        </div>
+    </div>;
+}
 
 class HierarchyDesigner extends DesignerContentbase { 
     
     render() {
         console.log('render hierarchyDesigner', this.state.leftPixels);
         return <NoobSplitter id="hierarchyDesigner" onDragEnd={this.onSplitDragEnd}>
-            <div>
+            {/* <div>
                 <HierarchyToolbar containerWidth={this.state.leftPixels}/>
                 <HierarchyDesignerTree/>
-            </div>
+            </div> */}            
+            <ToolbarPanel 
+                title="Hello" 
+                panelContent={<HierarchyDesignerTree/>}
+            />
             <div>Right</div>
         </NoobSplitter>
 
