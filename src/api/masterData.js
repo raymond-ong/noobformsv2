0fa84b2797859cf59c3d80981a36763b968627a4
Raymond Ong <remondo@gmail.com>
2020-01-17 18:19:58 +0800

    Make this site accessible from other PC's

--- src/api/masterData.js
+++ src/api/masterData.js
@@ -1,6 +1,17 @@
 import axios from 'axios';
 
+const getDocUrlWithoutPort = () => {
+    const temp = document.URL.replace("http://", '');
+    var toks = temp.split(':');
+    if (toks && toks.length > 0) {
+        return `http://${toks[0]}:60000/api`;
+    }
+
+    return null;
+}
+
 export default axios.create({
-    baseURL: 'http://localhost:60000/api'
+    //baseURL: 'http://localhost:60000/api'
+    baseURL: getDocUrlWithoutPort() || 'http://localhost:60000/api'
     //baseURL: 'http://localhost:3001'
 });
